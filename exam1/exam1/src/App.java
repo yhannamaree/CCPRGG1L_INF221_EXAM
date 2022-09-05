4a0a7a05238a776942328369e6d1e24c8c13125a
yhannamaree <111513727+yhannamaree@users.noreply.github.com>
2022-09-05 10:36:49 +0800

    Add files via upload

---
+++ exam1/exam1/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) {
+        
+      Student me = new Student ();
+    
+     
+      me.surname = "Leanna";
+      me.firstName = "Leanna Maree";
+      me.middleInitial = 'F';
+      me.dateOfBirth = 4/18/2004;
+      me.studentNumber = "2022-100992";
+      me.studentEmailAddress = "pillardalf@students.national-u.edu.ph";
+      me.iAmAwesome = true;
+      me.sayMyStudentNumber();
+      me.sayMyStudentEmailAddress();
+      me.iAmAwesome();
+      
+
+
+
+      
+    }
+}
---
+++ exam1/exam1/src/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    
+   String surname;
+   String firstName;
+   char middleInitial;
+   int dateOfBirth;
+   String studentNumber;
+   String studentEmailAddress;
+   boolean iAmAwesome;
+
+
+    public void sayMyStudentNumber() {
+        System.out.println("My studentNumber: " + studentNumber);
+    }
+
+            public void sayMyStudentEmailAddress() {
+                System.out.println("My studentEmailAddress: " + studentEmailAddress );
+            }
+                    public void iAmAwesome() {
+                        System.out.println("iAmAwesome: " + iAmAwesome );
+}
+
+}
